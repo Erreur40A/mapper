cd2d79c62dd6f7f27cea00696d788c2ac6d1ccc8
kali <kali@kali>
2023-12-26 16:56:45 +0100

    ajout des schemas steps__

---
+++ paris/sql/schemas/steps_rail_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE steps_rail(
+	from_stop_I INTEGER,
+	to_stop_I INTEGER,
+	route_I INTEGER,
+	route_count INTEGER,
+	route_index INTEGER
+);
---
+++ paris/sql/schemas/steps_subway_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE steps_subway(
+	from_stop_I INTEGER,
+	to_stop_I INTEGER,
+	route_I INTEGER,
+	route_count INTEGER,
+	route_index INTEGER
+);
