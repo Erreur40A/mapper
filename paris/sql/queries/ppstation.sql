db83af19950e93a886e8e1509a8a60f866b9efb7
Jibril DOUZI <jibril.douzi@edu.univ-paris13.fr>
2023-12-27 12:38:00 +0100

    requete pour trouver la plus proche station

--- paris/sql/queries/ppstation.sql
+++ paris/sql/queries/ppstation.sql
@@ -1,5 +1,5 @@
 --plus proche station par rapport a {lng} et {lat}
 
-SELECT name, (sqrt(pow(({lng}-nodes.lng), 2) + pow(({lat}-nodes.lat), 2))) AS dist 
+SELECT name, (SQRT(POW(({lng}-nodes.lng), 2) + POW(({lat}-nodes.lat), 2))) AS dist 
 FROM nodes 
 ORDER BY dist ASC;
