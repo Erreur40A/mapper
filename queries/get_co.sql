d975376bb32df3fa1e77f7c0e7caeea2f9cd80a0
Jibril DOUZI <jibril.douzi@edu.univ-paris13.fr>
2023-12-29 15:28:20 +0100

    correction syntaxe

--- queries/get_co.sql
+++ queries/get_co.sql
@@ -1,14 +1,15 @@
 --recupère les coordonnées d'un arret {name} avec le nom de sa ligne {n} et le 
 --mode de transport utiliser {pt}
+--ville choisit {ville}
 WITH tab1(stop_I, lat, lng) AS (
     SELECT stop_I, lat, lng
-    FROM nodes
+    FROM nodes_{ville}
     WHERE name={name}),
    
     tmp (route_I) AS
     (SELECT route_I
-    FROM route
+    FROM route_{ville}
     WHERE route_name={n})
 
 SELECT DISTINCT B.lat, B.lng
-FROM ({pt} AS A INNER JOIN tab1 AS B ON (A.from_stop_I=B.stop_I)) INNER JOIN tmp AS C ON (A.route_I=C.route_I);
+FROM (steps_{pt}_{ville} AS A INNER JOIN tab1 AS B ON (A.from_stop_I=B.stop_I)) INNER JOIN tmp AS C ON (A.route_I=C.route_I);
--- queries/ppstation.sql
+++ queries/ppstation.sql
@@ -1,6 +1,8 @@
---plus proche station par rapport a {lng} et {lat} en fonction du mode de transport chosit {pt}
+--plus proche station par rapport a {lng} et {lat} 
+--en fonction du mode de transport chosit {pt}
+--et de la ville choisit
 
-SELECT DISTINCT name, (SQRT(POW(({lng}-nodes.lng), 2) + POW(({lat}-nodes.lat), 2))) AS dist 
-FROM nodes, {pt}
-WHERE nodes.stop_I={pt}.from_stop_I OR nodes.stop_I={pt}.to_stop_I
+SELECT DISTINCT name, (SQRT(POW(({lng}-A.lng), 2) + POW(({lat}-A.lat), 2))) AS dist 
+FROM nodes_{ville} AS A, steps_{pt}_{ville} AS B
+WHERE A.stop_I=B.from_stop_I OR A.stop_I=B.to_stop_I
 ORDER BY dist ASC;
