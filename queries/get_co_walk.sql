4927f1fd796125f8fe342f47d2a2fe0359a5dc63
Erreur40A <152424147+Erreur40A@users.noreply.github.com>
2023-12-31 00:38:03 +0100

    pour double clique sur walk

---
+++ queries/get_co_walk.sql
@@ -0,0 +1,7 @@
+WITH tab1(stop_I, lat, lng) AS (
+    SELECT stop_I, lat, lng
+    FROM nodes_{ville}
+    WHERE name={name})
+
+SELECT DISTINCT B.lat, B.lng
+FROM (steps_{pt}_{ville} AS A INNER JOIN tab1 AS B ON (A.from_stop_I=B.stop_I))
